d47c16fb2ac9e6357dcae6f9dc10fc9b27766e27
Vasianandcarps <90087480+Vasianandcarps@users.noreply.github.com>
2021-11-13 11:54:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function initCounter(name) {
+    let color = "green";
+    let innerCounter = function () {
+      //console.log(name + " " + color);
+      if (color == "red") color = "yellow";
+      else if (color == "yellow") color = "green";
+      else color = "red";
+      return name + " " + color;
+    };
+    return innerCounter;
+  }
+  
+  let counter1 = initCounter("first"); // первый счетчик
+  console.log(counter1()); // 1
+  console.log(counter1());
+  console.log(counter1());
+  console.log(counter1());
+  // alert(counter1()); // 2
+  // alert(counter1()); // 3
+  
+  let counter2 = initCounter("second"); // второй счетчик
+  console.log(counter2()); // 1 - второй счетчик независим от первого
+  //alert(counter1()); // 4 - первый счетчик продолжает считать независимо от второго!
+  console.log(counter2());
+  console.log(counter2());
+  console.log(counter1());
+  console.log(counter2());
